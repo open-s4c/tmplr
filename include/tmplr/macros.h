8f815d4ba847968d7a7a64d1eddd1ca0e7b4f216
Diogo Behrens <diogo.huawei.com>
2025-09-22 07:59:40 +0200

    Make copyright less confusing
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- include/tmplr/macros.h
+++ include/tmplr/macros.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * Copyright (C) 2025 Huawei Technologies Co., Ltd.
  * SPDX-License-Identifier: MIT
  */
 #ifndef TMPLR_MACROS_H
